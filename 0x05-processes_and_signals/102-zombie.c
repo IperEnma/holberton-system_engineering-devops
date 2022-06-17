b339bc93b3989d23987d787ff736273e436f50c4
IperENma <enmanuelhernandez1843@gmail.com>
2022-06-17 08:50:33 -0700

    process

--- 0x05-processes_and_signals/102-zombie.c
+++ 0x05-processes_and_signals/102-zombie.c
@@ -3,6 +3,19 @@
 #include <stdlib.h>
 #include <sys/types.h>
 /**
+ * infinite_while - infinite loop
+ *
+ * Return: void
+ */
+int infinite_while(void)
+{
+	while (1)
+	{
+		sleep(1);
+	}
+	return (0);
+}
+/**
  * main - function principal
  *
  * Return: void
@@ -20,6 +33,6 @@ int main(void)
 		printf("Zombie process created, PID: %d\n", child_pid);
 		i++;
 	}
-	sleep(100);
+	infinite_while();
 	return (0);
 }
