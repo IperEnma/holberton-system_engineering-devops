533a177ce85e39beb001b0b582b75232cc88eae6
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-06 16:57:28 -0300

    user and title

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -5,14 +5,19 @@ import requests
 response_user = requests.get('https://jsonplaceholder.typicode.com/users/2')
 user = response_user.json()
 user_id = user['id']
-response_posts = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id))
+response_posts = requests.get(
+        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+            user_id))
 posts = response_posts.json()
 tasks = len(posts)
 completed = 0
 for _dict in posts:
     if _dict['completed'] is True:
         completed += 1
-print("Employee {} is done with tasks({}/{}):".format(user['name'], completed, tasks))
+print("Employee {} is done with tasks({}/{}):".format(
+    user['name'],
+    completed,
+    tasks))
 for _dict in posts:
-    if _dict['completed'] == True:
+    if _dict['completed'] is True:
         print("     {}".format(_dict['title']))
