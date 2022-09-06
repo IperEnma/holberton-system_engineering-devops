43fe22c25b219eb8bfc0565f59302c7081f10d3e
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-06 18:40:34 -0300

    reque

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -9,10 +9,14 @@ if __name__ == '__main__':
 
     user_id = argv[1]
 
-    response_user = requests.get('https://jsonplaceholder.typicode.com/users/{}'.format(user_id))
+    response_user = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(
+                user_id))
     user = response_user.json()
     name = user.get('name')
-    response_posts = requests.get('https://jsonplaceholder.typicode.com/todos?userId={}'.format(user_id))
+    response_posts = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                user_id))
     posts = response_posts.json()
 
     for dict_posts in posts:
@@ -23,4 +27,4 @@ if __name__ == '__main__':
         from_csv.append(str(dict_posts.get('title')))
         with open("{}".format(user_id) + ".csv", "a") as file:
             writer = csv.writer(file, dialect='unix')
-            writer.writerow(from_csv)
+            writer.writerow(send)
