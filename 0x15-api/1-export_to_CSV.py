8e6435dc0a13d8c9ed25d620107ecf7b7230624d
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-06 18:47:20 -0300

    csv

--- 0x15-api/1-export_to_CSV.py
+++ 0x15-api/1-export_to_CSV.py
@@ -20,11 +20,6 @@ if __name__ == '__main__':
     posts = response_posts.json()
 
     for dict_posts in posts:
-        from_csv = []
-        from_csv.append(str(user_id))
-        from_csv.append(str(name))
-        from_csv.append(str(dict_posts.get('completed')))
-        from_csv.append(str(dict_posts.get('title')))
         with open("{}".format(user_id) + ".csv", "a") as file:
             writer = csv.writer(file, dialect='unix')
-            writer.writerow(send)
+            writer.writerow([user_id, name, dict_posts.get('completed'), dict_posts.get('title')])
