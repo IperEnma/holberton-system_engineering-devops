309420b9fcc9641c564edac1dd3c38bc5f75e7dc
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-06 17:16:39 -0300

    if main

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -4,24 +4,26 @@ import requests
 from sys import argv
 
 
-response_user = requests.get(
-        'https://jsonplaceholder.typicode.com/users/{}'.format(
-            argv[1]))
-user = response_user.json()
-user_id = user.get('id')
-response_posts = requests.get(
-        'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
-            user_id))
-posts = response_posts.json()
-tasks = len(posts)
-completed = 0
-for _dict in posts:
-    if _dict.get('completed') is True:
-        completed += 1
-print("Employee {} is done with tasks({}/{}):".format(
-    user['name'],
-    completed,
-    tasks))
-for _dict in posts:
-    if _dict.get('completed') is True:
-        print("\t {}".format(_dict['title']))
+if __name__ == '__main__':
+    response_user = requests.get(
+            'https://jsonplaceholder.typicode.com/users/{}'.format(
+                argv[1]))
+    user = response_user.json()
+    user_id = user.get('id')
+    response_posts = requests.get(
+            'https://jsonplaceholder.typicode.com/todos?userId={}'.format(
+                user_id))
+    posts = response_posts.json()
+    tasks = len(posts)
+    completed = 0
+    for _dict in posts:
+        if _dict.get('completed') is True:
+            completed += 1
+    print("Employee {} is done with tasks({}/{}):".format(
+        user['name'],
+        completed,
+        tasks))
+    for _dict in posts:
+        if _dict.get('completed') is True:
+            print("\t {}".format(_dict['title']))
+
