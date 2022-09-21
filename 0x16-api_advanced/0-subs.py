b4ef69a5845ea13920544b1a422cc3d6d099e96c
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-21 15:07:05 -0300

    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

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -2,14 +2,16 @@
 """returns the number of subscribers"""
 import requests
 
+
 def number_of_subscribers(subreddit):
     """returns the number of subscribers"""
 
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
 
     headers = {
-        'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0',
-        'From': 'enmanuelhernandez1843@gmail.com'  # This is another valid field
+        'User-Agent':
+        'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0',
+        'From': 'enmanuelhernandez1843@gmail.com'
     }
     try:
         response = requests.get(url, headers=headers)
