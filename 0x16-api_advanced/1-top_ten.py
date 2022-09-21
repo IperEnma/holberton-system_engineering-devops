ae8656eaff381b4708df1910e828137b594a78ad
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-21 15:46:47 -0300

    none missing

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -13,9 +13,12 @@ def top_ten(subreddit):
         'Mozilla/5.0 (X11; Linux x86_64; rv:60.0) Gecko/20100101 Firefox/81.0',
         'From': 'enmanuelhernandez1843@gmail.com'
     }
-    response = requests.get(url, headers=headers)
-    response_json = response.json()
-    for idx in range(len(response_json['data']['children'])):
-        print(response_json['data']['children'][idx]['data']['title'])
-        if idx == 9:
-            break
+    try:
+        response = requests.get(url, headers=headers)
+        response_json = response.json()
+        for idx in range(len(response_json['data']['children'])):
+            print(response_json['data']['children'][idx]['data']['title'])
+            if idx == 9:
+                break
+    except Exception:
+        print(None)
