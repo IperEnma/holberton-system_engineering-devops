0556a1f698facce3ab0cf9b2d4b8a938533e16a6
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-22 14:00:58 -0300

    permissions

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -2,3 +2,7 @@ exec {"web debugging":
 command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
 provider => shell,
 }
+exec {"apache start":
+command => "/etc/init.d/apache2 restart",
+provider => shell,
+}
