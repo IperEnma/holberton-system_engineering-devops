c2a4f7734012b4af50d975aba8f030746e510594
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-22 14:08:55 -0300

    style fix

--- 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
+++ 0x17-web_stack_debugging_3/0-strace_is_your_friend.pp
@@ -1,8 +1,8 @@
-exec {"web debugging":
-command => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
-provider => shell,
+exec {'web debugging':
+  command  => "sed -i 's/class-wp-locale.phpp/class-wp-locale.php/g' /var/www/html/wp-settings.php",
+  provider => shell,
 }
-exec {"apache start":
-command => "/etc/init.d/apache2 restart",
-provider => shell,
+exec {'apache start':
+  command  => '/etc/init.d/apache2 restart',
+  provider => shell,
 }
