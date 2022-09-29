584100621ec13b5a7e3a674217be943e6bd58231
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-29 10:05:04 -0300

    comma

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
 # setting manually nginx without systemctl
 exec { 'replace ulimit value':
   	command  => 'sed -i "s/-n 15/-n 4096/g" /etc/default/nginx',
-	provider => shell
+	provider => shell,
 }
 exec { 'restart nginx':
   	command  => '/etc/init.d/nginx restart',
-	provider => shell
+	provider => shell,
 }
