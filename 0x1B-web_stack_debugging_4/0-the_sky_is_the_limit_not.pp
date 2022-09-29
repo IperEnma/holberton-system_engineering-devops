96489d7e3db904944a0ead82f3e8bc9822ebae8f
IperEnma <enmanuelhernandez1843@gmail.com>
2022-09-29 10:38:47 -0300

    tab

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,9 +1,9 @@
-#setting manually nginx
+#setting manually
 exec {'replace ulimit value':
-  	command  => "sed -i 's/-n 15/-n 7000/g' /etc/default/nginx",
-	provider => shell,
+  command  => "sed -i 's/-n 15/-n 4096/g' /etc/default/nginx",
+  provider => shell,
 }
 exec {'restart nginx':
-  	command  => '/etc/init.d/nginx restart',
-	provider => shell,
+  command  => '/etc/init.d/nginx restart',
+  provider => shell,
 }
